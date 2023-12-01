11602e8f70fa7d53a90b9947e3ca6c37499a5826
YuriGardinazzi <gardinazzi.yuri@gmail.com>
2023-12-01 12:51:22 +0100

    Update with numpy and pandas

--- test-python.py
+++ test-python.py
@@ -1,10 +1,13 @@
 #!/bin/python3 
 
 import sys
-
+import pandas as pd
+import numpy as np
 def say_hello():
     print("Ciao mondo, dalla pipeline e da dentro una funzione")
     print(f"version: {sys.version}")
+    df = pd.DataFrame(np.random.randint(0,100,size=(100, 4)), columns=list('ABCD'))
+    print(df.head())
 
 if __name__ == "__main__":
     say_hello()
