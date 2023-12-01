6cd3b0781467dc3ac6cd952ba3bd05233125d689
YuriGardinazzi <gardinazzi.yuri@gmail.com>
2023-12-01 12:43:14 +0100

    update with version inside the output

--- test-python.py
+++ test-python.py
@@ -1,8 +1,10 @@
 #!/bin/python3 
 
+import sys
 
 def say_hello():
     print("Ciao mondo, dalla pipeline e da dentro una funzione")
+    print(f"version: {sys.version}")
 
 if __name__ == "__main__":
     say_hello()
